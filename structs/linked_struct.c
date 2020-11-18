0b6aea91e59dbe5fb8fda0bb189746ca22329604
Eudes Serpa <serpagame@gmail.com>
2020-11-17 21:14:29 -0500

    Cambio de nombre a funcion inser_nodo por append. Se agg la funcion insert (sin aplicacion por ahora pero con buen funcionamiento individual). Se agg funcion para liberar memoria de las lists para estudiantes y con ello se arregla error en la liberacion de memoria de las lista de docentes.

--- structs/linked_struct.c
+++ structs/linked_struct.c
@@ -24,7 +24,8 @@ Nodo *footer = NULL;
 Student *last = NULL;
 
 Nodo *create_nodo(Student **ptrAlumnxDoc);
-void insert_nodo(Nodo *nodo);
+void append(Nodo *nodo);
+void insert(Nodo *nodo, int pos);
 void show_list(Student **ptrAxD);
 void free_memory();
 
@@ -32,24 +33,25 @@ int main(void){
 
  	unsigned	char teachers = 0, i = 0;
 	Nodo *ptrDocentes = NULL;
-	Student **ptrAlumnxDoc = NULL; //Vector de punteros Student *ptrAlumnxDoc[0]; 
+	Student **ptrAlumnxDoc = NULL; //Array de punteros: Student *ptrAlumnxDoc[0]; 
 
 	printf("Write the numbers of teachers to assign: ");
 	scanf("%u",&teachers);
-	while(getchar() != '\n'); //Limpiar buffer (Sino, se toma un \n para el 1er nombre del 1er profesor.)
+	while(getchar() != '\n'); //Limpiar buffer (Sino, se toma un \n para el 1er nombre del 1er profesor)
 
-	ptrDocentes = (Nodo *) malloc(sizeof(Nodo) * teachers); //Array of pointers for nodos
-	ptrAlumnxDoc = (Student **) malloc(sizeof(Student) * teachers); //Array of pointers for alumns x teachers
+	ptrDocentes = (Nodo *) malloc(sizeof(Nodo) * teachers); //Array de tipo Nodo
+	ptrAlumnxDoc = (Student **) malloc(sizeof(Student) * teachers);
 
 	for(i=0; i<teachers; i++){
 		printf("Docente %d\r\n", i+1);
-		Nodo *nodo1 = create_nodo(ptrAlumnxDoc); //*ptrAlumnxDoc
+		Nodo *nodo1 = create_nodo(ptrAlumnxDoc); 
 		*(ptrDocentes + i) = *nodo1; //Location stored in array
 	}
 
 	for(i=0; i<teachers; i++)
-		insert_nodo(ptrDocentes + i);
-	
+		append(ptrDocentes + i);
+	//insert(ptrDocentes+i, 0); //WARNING: Los alumnos no cambian de posicion si se trabaja asi.
+
 	show_list(ptrAlumnxDoc);
 
 	free_memory();
@@ -61,9 +63,9 @@ Nodo *create_nodo(Student **ptrAlumnxDoc){
 	/* Crea los nuevos nodos e inicializa sus miembros.
 		Args:
 			- Student **ptrAxD: Vector de punteros la estructura Student. 
-								  Estos almacenan los punteros al header de listas de alumnos para cada profesor.
+								     Almacena los punteros al header de cada list de alumnos por profesor.
 		Return:
-			- Puntero al nuevo nodo.
+			- Puntero al nodo creado.
 	*/	
 	char *data(); //Prototipo
 	static char y=0;
@@ -88,15 +90,15 @@ Nodo *create_nodo(Student **ptrAlumnxDoc){
 		Student *new_alumn = (Student *) malloc(sizeof(Student));
 		
 		printf("Write the Alumn's name: ");
-		new_alumn->ptrNombre = data();
+		new_alumn->ptrNombre = data(); //Inicializar nodo del alumno.
 		new_alumn->ptrNextAlumn = NULL;
 			
-		if(i == 0){
+		if(i == 0){//First alumn (first nodo)
 			new_nodo->ptrStudent = new_alumn; //El miembro de la struct padre apunta al primer nodo de la list alumns
 			*(ptrAlumnxDoc + y) = new_alumn; //Almacena la dir del inicio de la list de alumns. 
 			last = new_alumn;
 			y++;
-		}else{
+		}else{//Others alumns. insert to end.
 			last->ptrNextAlumn = new_alumn;
 			last = new_alumn;
 		}
@@ -110,21 +112,61 @@ Nodo *create_nodo(Student **ptrAlumnxDoc){
 char *data(){
 	/* Lectura dinamica de data. 
 		Return:
-	   		Puntero que apunta al texto leido.*/
+	   		Puntero al texto leido
+	*/
 	size_t n_bytes = 0;
 	size_t size_line = 0;
 	char *ptrName = NULL;
 
 	if ((size_line = getline(&ptrName, &n_bytes, stdin)) == -1)
 		puts("ERROR: EOF found wihtout reading any byte / Others errors");
-	else
+	else //WARNING: Existen saltos de lineas. Se deben Eliminar
 		return ptrName;
 }
 
-void insert_nodo(Nodo *nodo){ 
-	/* Inserta el nodo especificado en el parametro de esta funcion al final de la list.
+void insert(Nodo *nodo, int pos){
+	/* Inserta un nodo en la lista, en el indice especificado.
 		Args:
-   			- Nodo *nodo: Puntero al tipo de dato Nodo.		
+			- Nodo *nodo: Puntero a la estructura Nodo. Nodo a insertar en la lista.
+			- int pos: Indice para posicion de insercion del nodo.
+	*/
+	unsigned char n = 0;
+	Nodo *ptrant = NULL;
+	Nodo *ptractual = header;
+
+	if(header == NULL){
+		puts("Empty list. Insert to start (pos=0)");
+		append(nodo); //Insecion al inicio de la lista.
+		return;
+	}
+
+	if(pos != 0){ //pos>0 con nodos en lista. Insercion entre nodos.
+		while(ptractual != NULL){
+			if(n == pos){
+				ptrant->ptrNext = nodo;
+				nodo->ptrNext = ptractual;
+				return;
+			}else{
+				ptrant = ptractual;
+				ptractual = ptractual->ptrNext;
+				n++;
+			}
+		}
+		puts("Posicion mayor a los nodos actuales en la lista, insert to end");
+		append(nodo); //Insercion al final de la lista. 
+		n = 0;
+		return;
+	}else{ //pos=0 con al menos 1 nodo en la lista. Insercion al inicio.
+		header = nodo;
+		header->ptrNext = ptractual;
+		return;
+	}
+}
+
+void append(Nodo *nodo){ 
+	/* Inserta un nodo al final de la lista de docentes.
+		Args:
+   			- Nodo *nodo: Puntero al tipo de dato Nodo. Nodo a insertar.		
 	*/
 	if(header == NULL){ //First nodo. Es la cabeza y fin de la lista
 		header = nodo;
@@ -136,10 +178,10 @@ void insert_nodo(Nodo *nodo){
 }
 
 void show_list(Student **ptrAxD){
-	/*Recorre la lista y muestra los datos de sus nodos.
+	/* Recorre la lista de docentes y de sus estudiantes para muestrar los datos de sus nodos.
 		Args: 
 			- Student **ptrAxD: Vector de punteros la estructura Student. 
-									  Estos almacenan los punteros al header de listas de alumnos para cada profesor.
+									  Almacenan los punteros al header de listas de alumnos para cada profesor.
 	*/
 	Nodo *scroll = header;
 	unsigned char i = 0, len=0;
@@ -152,11 +194,11 @@ void show_list(Student **ptrAxD){
 		puts("[");
 		while(scroll != NULL){
 			puts("\t{");
-			printf("\t\tNombre: %s\r\n\t\tApellido: %s\r\n\t\t", scroll->ptrNombre, scroll->ptrApellido);			
-			printf("Edad: %s\r\n\t\tPeso: %s\r\n\t\t", scroll->ptrEdad, scroll->ptrPeso);			
+			printf("\t\tNombre: %s\r\n\t\tApellido: %s\r\n", scroll->ptrNombre, scroll->ptrApellido);
+			printf("\t\tEdad: %s\r\n\t\tPeso: %s\r\n\t\t", scroll->ptrEdad, scroll->ptrPeso);			
 			printf("Alumnos: [");
 			while(*(ptrAxD+i) != NULL){
-				len = strlen((*(ptrAxD+i))->ptrNombre);
+				len = strlen((*(ptrAxD+i))->ptrNombre);//Lenght of names.
 				if((*(ptrAxD+i))->ptrNombre[len - 1] == '\n') //Eliminar saltos de linea
 					(*(ptrAxD+i))->ptrNombre[len - 1] = '\0';
 				printf("%s, ", (*(ptrAxD+i))->ptrNombre); //v[0+i] = ptr = &var ---> *v[0+i] == var 
@@ -172,7 +214,10 @@ void show_list(Student **ptrAxD){
 }
 
 void free_memory(){
-	/* Libera la memoria asignada para cada nodo insertado en la lista.*/
+	/* 
+		Libera la memoria asignada para la lista de docentes
+	*/
+	void free_student(Nodo *scroll); 
 	Nodo *scroll = header;
 	
 	if(scroll == NULL){
@@ -181,12 +226,27 @@ void free_memory(){
 	}else{
 		while(scroll != NULL){
 			header = scroll->ptrNext; //Update header of list
+			free_student(scroll);
 			free(scroll);
-			//free(scroll->ptrStudent);
 			scroll = header;
 		}
 		header = NULL;
 		footer = NULL;
-		puts("\r\nMemory freed");
+		puts("\r\nMemory teachers structs freed");
+	}
+}
+
+void free_student(Nodo *scroll){
+	/* 
+		Libera la memoria asignada para la lista de estudiantes
+	*/
+	if(scroll == NULL){
+		puts("\r\nWARNING: No se han insertado nodos a la lista para liberar memoria automaticamente.");
+		return;
+	}else{
+		free(scroll->ptrStudent);
+		header = NULL;
+		footer = NULL;
+		puts("Memory students structs freed\r\n");
 	}
 }
